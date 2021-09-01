49623be044577b11c481d0d319834f0bfb9512c4
dmitry <d.fokin@mobidev.biz>
2021-09-01 08:58:38 +0300

    Fix

--- problem1/index.js
+++ problem1/index.js
@@ -1,16 +1,20 @@
 /*
 * Let`s say x = 7
-* the result = x + 2x + 3x ..... + a*x, where a * x < 1 000 000
-* 1 step - find a nearest number to 1 000 000 which divides by 7 but not equal 1 000 000
-* 2nd step 999 999 / 7 = factor
-* now we have a factor
+* the result for x = 7 = x + 2x + 3x ..... + a*x, where a * x < 1 000 000
+* 1 step - find a nearest number to 1 000 000 which divides by 7 but not equal 1 000 000 - a
+* factor = a / divider = a / 7
+* result = divider + 2*divider + 3*divider ..... + divider * factor = divider * (1 + 2 + 3 + ... + factor)
+* the same process for 5
+*
+* Last thing - get rid of duplicates, numbers which divides by 7 and 5 repeats in sequence,
+* we do the same process for 5 * 7 divider and subtract the result from our result sum
 * */
 
 
 
 
 
-function sumOfArithmeticProgression(n) { //for numbers less than n
+function sumOfArithmeticProgression(n) {
     return ((1 + n) / 2) * n;
 }
 
@@ -48,8 +52,8 @@ function main() {
     const firstDivider = 5;
     const secondDivider = 7;
 
-    // const n = 1000000
-    const n = 40
+    const n = 1000000
+    // const n = 40
 
     const s1 = solve(firstDivider, n);
 
